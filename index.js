a01e9a2e75ac04511d93b20aab9f29fc23454a90
Ali Omar <ali.noor.student.moringaschool.com git config --gobal Ali abdullahigit config --global user.email ali.noor.student.moringaschool.com>
2022-12-05 15:03:23 +0300

    lab done

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+//returns a year the denver broncos won the superbowl
+//returns undefined as the year if the record has no win objects
+
+function superbowlWin(record){
+    let winYear = record.find(function (win){
+    return win.result === "W"})
+    if (winYear !== undefined){
+        return winYear.year
+    } else {
+        return undefined
+    }
+
+}superbowlWin(record)
